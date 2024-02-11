d81468e467ddccd5279760692ec43813182d8ce7
Семён <karachinstar@yandex.ru>
2024-02-11 10:51:45 +0800

    ф

---
+++ m2_layout/app/src/main/java/ru/gb/android/hw2/m2_layout/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.gb.android.hw2.m2_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
